f5f3e8558873e484f92391a7def7dc012d35dee2
kurapati.manisha <kurapatimanisha7@gmail.com>
2026-05-14 23:16:48 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,25 @@
+#   list = used to store multiple items in a single variable
+
+#food=["chiken","biryani","aloo fry","curd"]
+#       0          1          2        3
+#print(food)
+#print(food[1])
+#food[3]="Buttermilk"
+#print(food[3])
+
+#for i  in  food:
+#    print(i)
+
+#food.append("ice cream")    #adds a item at the end of  list 
+
+#food.remove("aloo fry")     #removes item from the list
+
+#food.pop()                   # remove the  last item from the list
+
+#food.insert(0,"cake")         #add a item into the lis
+
+#food.sort()                   # sort the list according to alphabatical order
+
+#food.clear()                  # delete the elements from list
+#for i  in  food:
+#   print(i)
---
+++ loop control statements.py
@@ -0,0 +1,24 @@
+#  loop control statements=change loop execution  from its normal sequence
+#         break:  used to terminate the loop entirely
+#         continue:   skip the next iteration of the loop
+#         pass:   does nothing,acts as placeholder
+  
+
+
+#while True:
+#    name=input("Enter your name:")
+#    if name!="":
+#        break
+
+#Aadhar_number="1234-5678-9012"
+#for i in Aadhar_number:
+#    if i=="-":
+#        continue
+#    print(i)
+
+for i in  range(1,11):
+    if i==6:
+        pass
+    else:
+        print(i)
+    
